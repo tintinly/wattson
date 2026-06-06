bac91947dd2e27166fd839097b1ebde426b5f139
tintinly <821294434@qq.com>
2026-06-06 22:22:15 +0800

    通过box-shadow美化时间线样式、对齐虚线

--- app/components/blog/Timeline.vue
+++ app/components/blog/Timeline.vue
@@ -1,55 +1,48 @@
 <template>
-  <div class="py-5 px-5 sm:py-8 sm:px-8 bg-surface rounded-xl border border-border flex flex-col">
+  <div class="py-4 px-4 sm:py-7 sm:px-7 bg-surface rounded-xl border border-border flex flex-col">
     <p v-if="!timelineItems.length" class="text-center text-foreground-secondary py-20">{{ t('archive.empty') }}</p>
     <div v-for="(item, index) in timelineItems" :key="index">
       <!-- 年份标题 -->
-      <div
-        v-if="item.type === 'year'"
-        class="flex flex-row h-12 items-center"
-      >
-        <div class="w-[15%] lg:w-[10%] text-right text-2xl font-bold select-none">
-            {{ item.label }}
+      <div v-if="item.type === 'year'" class="flex flex-row h-15 items-center text-foreground">
+        <div class="w-[20%] lg:w-[10%] text-right text-2xl font-bold select-none">
+          {{ item.label }}
         </div>
         <div class="w-[15%] lg:w-[10%] relative  h-full flex items-center">
-          <div class="mx-auto w-2 h-2 rounded-full group-hover:h-5
-                       outline outline-3">
-            </div>
+          <div class="mx-auto w-2 h-2 rounded-full group-hover:h-5 
+              outline-3  z-10">
+          </div>
         </div>
       </div>
 
       <!-- 文章条目 -->
-      <NuxtLink
-        v-else
-        :to="localePath(`/posts/${item.slug}`)"
-        class="group flex flex-row justify-start items-center h-10 rounded-lg text-base text-foreground-secondary  hover:bg-background-secondary active:bg-background-secondary"
-      >
-        <div class="w-[15%] lg:w-[10%] text-sm text-right">
-            {{ item.dateStr }}
+      <NuxtLink v-else :to="localePath(`/posts/${item.slug}`)"
+        class="group flex flex-row justify-start items-center h-10 rounded-lg text-base  hover:bg-background-secondary active:bg-background-secondary  transition-all duration-300">
+        <div class="w-[20%] lg:w-[10%] text-xs text-right text-foreground-secondary/75">
+          {{ item.dateStr }}
         </div>
-        <div class="w-[15%] lg:w-[10%] relative h-full flex items-center
-                    before:content-[''] before:w-full before:h-full before:left-[50%] before:translate-y-[-50%] before:border-l-2 before:border-dashed before:absolute">
+        <div
+          class="w-[15%] lg:w-[10%] relative h-full flex items-center
+                    before:content-[''] before:w-full before:h-full before:left-[50%] before:translate-y-[-50%] before:translate-x-[-1px] before:border-l-2 before:border-dashed before:absolute before:border-foreground-secondary/25">
           <div class="mx-auto w-1 h-1 rounded-full group-hover:h-5
-                      bg-foreground-secondary transition-all duration-300">
+                      bg-foreground-secondary ring-4 ring-surface group-hover:ring-background-secondary
+                      relative z-10 transition-all duration-300">
           </div>
         </div>
         <div class="w-[65%] lg:w-[60%] ">
-          <h3 class="text-foreground font-bold group-hover:translate-x-2 truncate transition-all duration-300 ">
-            <span v-if="item.locale !== locale" class="inline-block px-1.5 py-0.5 border border-border text-foreground-secondary font-normal text-xs rounded-md bg-background-secondary">
+          <h3 class="text-foreground/80 font-bold text-sm sm:text-base group-hover:translate-x-2 truncate transition-all duration-300 ">
+            <span v-if="item.locale !== locale"
+              class="inline-block px-1.5 py-0.5 border border-border text-foreground-secondary font-normal text-xs rounded-md bg-background-secondary">
               {{ item.locale }}
             </span>
             {{ item.title }}
           </h3>
         </div>
-        <div class="hidden lg:block lg:w-[20%] text-sm text-right truncate px-2">
+        <div class="hidden lg:block lg:w-[20%] text-sm text-right truncate px-2  text-foreground-secondary/75">
           #
-          <span
-            v-for="tag in item.tags"
-            :key="tag"
-            class="ml-1 cursor-pointer"
-          >
+          <span v-for="tag in item.tags" :key="tag" class="ml-1 cursor-pointer">
             {{ tag }}
           </span>
-          
+
         </div>
       </NuxtLink>
     </div>
