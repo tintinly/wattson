66703ff6cab7cfc047d341419181135b3c4553ea
tintinly <821294434@qq.com>
2026-06-29 21:45:26 +0800

    归档分类标签栏UI调整

--- app/components/blog/CategoryFilter.vue
+++ app/components/blog/CategoryFilter.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="p-6 flex flex-wrap gap-2 bg-surface rounded-xl border border-border">
-    <button
-      class="text-sm px-2 py-1 rounded-lg border transition-all"
-      :class="[
-        selectedCategory === null
-          ? 'border-border-secondary bg-background-secondary'
-          : 'border-border text-foreground-secondary hover:border-border-secondary'
-      ]"
-      @click="$emit('select', null)"
-    >
-      {{ t('archive.allCategories') }}
-    </button>
-    <button
-      v-for="cat in categories"
-      :key="cat.name"
-      class="text-sm px-2 py-1 rounded-lg border transition-all"
-      :class="[
-        selectedCategory === cat.name
-          ? 'border-border-secondary bg-background-secondary'
-          : 'border-border text-foreground-secondary hover:border-border-secondary'
-      ]"
-      @click="$emit('select', cat.name)"
-    >
-      {{ cat.name }}
-    </button>
+  <div class="p-5 bg-surface rounded-xl border border-border">
+    <div class="relative ml-4 text-md font-bold mb-4 before:w-1 before:h-5 before:rounded-md before:bg-background-tertiary
+        before:absolute before:-left-4 before:top-px before:hidden md:before:block">
+        {{ t('archive.categories') }}
+    </div>
+    <div class="flex flex-wrap gap-2">
+      <button
+        class="text-sm px-2 py-1 rounded-lg border transition-all"
+        :class="[
+          selectedCategory === null
+            ? 'border-border-secondary bg-background-secondary'
+            : 'border-border text-foreground-secondary hover:border-border-secondary'
+        ]"
+        @click="$emit('select', null)"
+      >
+        {{ t('archive.allCategories') }}
+      </button>
+      <button
+        v-for="cat in categories"
+        :key="cat.name"
+        class="text-sm px-2 py-1 rounded-lg border transition-all"
+        :class="[
+          selectedCategory === cat.name
+            ? 'border-border-secondary bg-background-secondary'
+            : 'border-border text-foreground-secondary hover:border-border-secondary'
+        ]"
+        @click="$emit('select', cat.name)"
+      >
+        {{ cat.name }}
+      </button>
+    </div>
   </div>
 </template>
 
--- app/components/blog/TagFilter.vue
+++ app/components/blog/TagFilter.vue
@@ -1,31 +1,38 @@
 <template>
-  <div class="p-6 flex flex-wrap gap-2 bg-surface rounded-xl border border-border">
-    <button
-      class="text-sm px-2 py-1 rounded-lg border transition-all"
-      :class="[
-        selectedTag === null
-          ? 'border-border-secondary bg-background-secondary'
-          : 'border-border text-foreground-secondary hover:border-border-secondary'
-      ]"
-      @click="$emit('select', null)"
-    >
-      {{ t('archive.allTags') }}
-    </button>
-    <button
-      v-for="tag in tags"
-      :key="tag.name"
-      class="text-sm px-2 py-1 rounded-lg border transition-all"
-      :class="[
-        selectedTag === tag.name
-          ? 'border-border-secondary bg-background-secondary'
-          : 'border-border text-foreground-secondary hover:border-border-secondary'
-      ]"
-      @click="$emit('select', tag.name)"
-    >
-      {{ tag.name }}
-      <!-- <span class="text-xs text-foreground-secondary ml-1">({{ tag.count }})</span> -->
-    </button>
+  <div class="p-5 bg-surface rounded-xl border border-border">
+    <div class="relative ml-4 text-md font-bold mb-4 before:w-1 before:h-5 before:rounded-md before:bg-background-tertiary
+        before:absolute before:-left-4 before:top-px before:hidden md:before:block">
+        {{ t('archive.tags') }}
+    </div>
+    <div class="flex flex-wrap gap-2">
+      <button
+        class="text-sm px-2 py-1 rounded-lg border transition-all"
+        :class="[
+          selectedTag === null
+            ? 'border-border-secondary bg-background-secondary'
+            : 'border-border text-foreground-secondary hover:border-border-secondary'
+        ]"
+        @click="$emit('select', null)"
+      >
+        {{ t('archive.allTags') }}
+      </button>
+      <button
+        v-for="tag in tags"
+        :key="tag.name"
+        class="text-sm px-2 py-1 rounded-lg border transition-all"
+        :class="[
+          selectedTag === tag.name
+            ? 'border-border-secondary bg-background-secondary'
+            : 'border-border text-foreground-secondary hover:border-border-secondary'
+        ]"
+        @click="$emit('select', tag.name)"
+      >
+        {{ tag.name }}
+        <!-- <span class="text-xs text-foreground-secondary ml-1">({{ tag.count }})</span> -->
+      </button>
+    </div>
   </div>
+  
 </template>
 
 <script setup lang="ts">
